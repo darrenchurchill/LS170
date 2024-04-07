ca90f426067596deef6fba094709f3a1f19b00f0
Darren Churchill <darrenmchurchill@gmail.com>
2024-04-07 12:24:07 -0700

    Add 4.11: Working with the Browser
    - Read any number of lines in the request
    - Add Content-Type and Content-Length headers to responses

--- lesson_1/http-server.sh
+++ lesson_1/http-server.sh
@@ -4,23 +4,37 @@
 
 function response_200() {
     echo -ne 'HTTP/1.1 200 OK\r\n'
+    echo -ne 'Content-Type: text/html; charset=utf-8\r\n'
+    echo -ne "Content-Length: $(cat $1 | wc -c)\r\n"
     echo -ne '\r\n'
     cat "$1"
 }
 
 function response_400() {
     echo -ne 'HTTP/1.1 400 Bad Request\r\n'
+    echo -ne 'Content-Length: 0\r\n'
     echo -ne '\r\n'
 }
 
 function response_404() {
     echo -ne 'HTTP/1.1 404 Not Found\r\n'
+    echo -ne 'Content-Length: 0\r\n'
     echo -ne '\r\n'
 }
 
 function server () {
     while true; do
-        read method path version
+        message_arr=()
+        check=true
+        while $check; do
+            read line
+            message_arr+=($line)
+            if test "${#line}" -eq 1; then
+                check=false
+            fi
+        done
+        method=${message_arr[0]}
+        path=${message_arr[1]}
 
         if test "$method" = "GET"; then
             if test -r "./www/$path"; then
@@ -36,4 +50,4 @@ function server () {
 
 coproc SERVER_PROCESS { server; }
 
-nc -lv 2345 <&"${SERVER_PROCESS[0]}" >&"${SERVER_PROCESS[1]}"
+nc -lkv 2345 <&"${SERVER_PROCESS[0]}" >&"${SERVER_PROCESS[1]}"
